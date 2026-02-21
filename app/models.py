015561e3bffa7f7f9152a98fc9fa1427b7abc7a1
starra-dev <starra563@gamil.com>
2026-02-21 07:39:39 -0500

    basic change

--- app/models.py
+++ app/models.py
@@ -1,15 +1,7 @@
-from pydantic import BaseModel
-from enum import Enum
-from tkinter import time
 
 
 
-class Attendance(BaseModel):
-    name:str
-    id:int
-    department:str
-    checkin:bool
-    timestamp:time
-    
+
+
 
 
--- app/route.py
+++ app/route.py
@@ -1,16 +1,10 @@
-from fastapi import APIRouter
-import json
+from fastapi import FastAPI
 
 
-router = APIRouter()
 
-@router.get("/data")
-def load_data():
-     with open (data.json, "r") as f:
-        data = json.load()
+app = FastAPI()
 
 
-@router.post("/data")
-def save_data():
-    with open (data.json, "w") as f:
-        data = json.dump()
+
+
+
