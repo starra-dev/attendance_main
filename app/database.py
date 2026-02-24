a83caf0dbfb323024ae908e9ea0c611ba47712c6
starra-dev <starra563@gamil.com>
2026-02-24 10:38:46 -0500

    update

---
+++ app/database.py
@@ -0,0 +1,9 @@
+import sqlite3
+
+
+con = sqlite3.connect("attendance.db")
+
+cur = con.cursor()
+
+
+cur.execute(''' ''')
--- app/models.py
+++ app/models.py
@@ -1,5 +1,22 @@
+from enum import Enum
+from pydantic import BaseModel
+from datetime import datetime,date,time
+
+class Status(Enum):
+    EARLY =  time <= 9,
+    LATE = time > 9
 
 
+class Record(BaseModel):
+    name: str
+    date: date
+    status: Status
+
+
+class Profile(BaseModel):
+    name: str
+    email : str
+    password: str
 
 
 
